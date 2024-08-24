f40335a0bf086f053040e8858a14ac1441f45d58
Thiago de Melo Mota <82675235+Ghitado@users.noreply.github.com>
2024-08-24 01:01:34 +0000

    feat: criação componente para listar os produtos

--- app/Componentes/Adaptadores/ListaProdutos/index.tsx
+++ app/Componentes/Adaptadores/ListaProdutos/index.tsx
@@ -1,14 +1,17 @@
-import { View } from "react-native"
+import { View, ScrollView } from "react-native"
 import ItemProduto from "../ItemProduto"
 
-function ListaProdutos({produtos}:{produtos:[]}) {
+function ListaProdutos({produtos}: {produtos: 
+    {nome: string, preco: number}[]}) {
     return (
+        <ScrollView>
         <View>
             {produtos.map((p) => 
                 <ItemProduto produto={p}>
                     
                 </ItemProduto>)}
         </View>
+        </ScrollView>
     )
 }
 
