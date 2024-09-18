70b54ceb9e66620720dd6c43dde97c95793ad6ea
Ghitado <thiaguinho.020405@gmail.com>
2024-09-17 23:32:10 -0300

    feat: Botão de cadastrar e produto com mais propriedades à amostra

--- app/Componentes/Adaptadores/CadastroProduto/index.tsx
+++ app/Componentes/Adaptadores/CadastroProduto/index.tsx
@@ -1,18 +1,52 @@
-import { useState } from "react"
-import { Text, TextInput, View } from "react-native"
+import Produto from "@/app/Models/Produto";
+import Style from "@/app/Styles/Default";
+import axios from "axios";
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
-const CadastroProduto = ()=>{
-    let [nome,setNome] = useState('');
-    let [descricao,setDescricao] = useState('');
+const CadastroProduto = () => {
+  let [nome, setNome] = useState("");
+  let [descricao, setDescricao] = useState("");
+  let [preco, setPreco] = useState(0);
+  let [foto, setFoto] = useState("");
 
-    return(
-        <View>
-            <Text >Nome:</Text>
-            <TextInput onChangeText={setNome} value={nome} />
-            <Text >Descricao:</Text>
-            <TextInput onChangeText={setDescricao} value={descricao} />
-        </View>
-    )
-}
+  return (
+    <View style={Style.container}>
+      <Text>Nome:</Text>
+      <TextInput onChangeText={setNome} value={nome} style={Style.input} />
+
+      <Text>Descricao:</Text>
+      <TextInput onChangeText={setDescricao} value={descricao} style={Style.input}  />
+
+      <Text>Foto:</Text>
+      <TextInput onChangeText={setFoto} value={foto} style={Style.input}  />
+
+      <Text>Preço:</Text>
+      <TextInput
+        onChangeText={(v) => {
+          setPreco(isNaN(parseFloat(v)) ? 0 : parseFloat(v));
+        }}
+        inputMode="numeric"
+        value={preco.toString()}
+        style={Style.input} 
+      />
+
+      <Button title="Cadastrar" onPress={Cadastrar} />
+    </View>
+  );
+
+  function Cadastrar() {
+    let produto: Produto = {
+      nome: nome,
+      descricao: descricao,
+      preco: preco,
+      foto: foto,
+    } as Produto;
+    let api = "https://api-docker-2t8m.onrender.com/api/produtos";
+    axios.post(api, produto).then((resp) => {
+      setNome("");
+    });
+  }
+};
 
 export default CadastroProduto;
