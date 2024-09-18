5e7561abb2d05f336629886461be9bae57200bb6
Ghitado <thiaguinho.020405@gmail.com>
2024-09-17 23:30:04 -0300

    feat: Imagem do produto e botão excluir produto

--- app/Componentes/Adaptadores/ItemProduto/index.tsx
+++ app/Componentes/Adaptadores/ItemProduto/index.tsx
@@ -1,20 +1,41 @@
-import { View, Text, Image } from "react-native"
-import Style from "@/app/Styles/Default"
-import Produto from "@/app/Models/Produto"
+import Produto from "@/app/Models/Produto";
+import Style from "@/app/Styles/Default";
+import axios from "axios";
+import React from "react";
+
+import { Alert, Button, Image, Text, View } from "react-native";
 
 interface PropProd {
-    produto: Produto
+  produto: Produto;
+  aoExcluir?: Function;
 }
 
-const ItemProduto: React.FC<PropProd> = 
-        ({produto}) => {
-    return (
-        <View style={Style.card}>
-            <Text style={Style.cardText}>{produto.nome}</Text>
-            <Text style={Style.cardText}>{produto.preco}</Text>
-            <Image source={{uri:produto.foto}} style={Style.imagem}/>
-        </View>
-    )
-}
+const ItemProduto: React.FC<PropProd> = ({ produto, aoExcluir }) => {
+  console.log(produto);
 
-export default ItemProduto
+  function Excluir(id: number) {
+    let api = "https://api-docker-2t8m.onrender.com/api/produtos";
+    axios.delete(`${api}/${id}`).then((resp) => {
+      aoExcluir?.call(null);
+      Alert.alert("Produto excluido com sucesso");
+      alert("Produto excluido com sucesso");
+    });
+  }
+
+  return (
+    <View style={Style.card}>
+      <Text style={Style.cardText}>{produto.nome}</Text>
+      <Text style={Style.cardText}>{produto.preco}</Text>
+      <Text style={Style.cardText}>{produto.descricao}</Text>
+      <Image source={{ uri: produto.foto }} style={Style.image} />
+      <Button
+        title="Excluir"
+        onPress={() => {
+          Excluir(produto.id);
+        }}
+      />
+    </View>
+  );
+};
+
+export default ItemProduto;
